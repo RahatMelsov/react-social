cec674c75d113c25d8798dd18c1f80b31cf2cc67
Rahat Melsov <rahatmelsovforjob@gmail.com>
2021-04-17 14:16:36 +0600

    First commit the react project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header';
+import Navbar from './components/navbar';
+import Profile from './components/profile';
+
+const App = () => {
+  return (
+    <div className="app-wrapper">
+        < Header/>
+        < Navbar/>
+        < Profile/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Content = () => {
+	return (
+	<div className='content'>
+      
+      <div className="bg"><img src="https://i.pinimg.com/originals/21/a1/92/21a1926ddd463659b1b17a75bdb0c6af.png"/></div>
+      <div className="ava"><img src="https://s5o.ru/storage/simple/ru/edt/b1/c6/d7/e0/rue9685f06f6b.jpg"/></div>
+      
+      <div className='posts'>
+        My posts
+          <div>
+            New Post
+          </div>
+
+          <div>
+            Post 1
+          </div>
+
+          <div>
+            Post 2
+          </div>
+
+      </div> 
+
+      </div>
+	 
+      )
+}
+
+export default Content;
