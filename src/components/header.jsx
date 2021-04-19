89c5f0c321f2e07171afef607d4b5d76b3f76cd0
Rahat Melsov <rahatmelsovforjob@gmail.com>
2021-04-19 23:06:36 +0600

    create modules fot css

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import classes from './header.module.css';
 
 const Header = () => {
-	return (<header className='header'>
+	return (<header className={classes.header}>
 	        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/512px-LEGO_logo.svg.png"/>
 	</header>)
 }
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
+import classes from './profile.module.css';
 
 const Content = () => {
 	return (
-	<div className='content'>
+	<div className={classes.content}>
       
-      <div className="bg"><img src="https://i.pinimg.com/originals/21/a1/92/21a1926ddd463659b1b17a75bdb0c6af.png"/></div>
-      <div className="ava"><img src="https://s5o.ru/storage/simple/ru/edt/b1/c6/d7/e0/rue9685f06f6b.jpg"/></div>
+      <div className={classes.bg}><img src="https://i.pinimg.com/originals/21/a1/92/21a1926ddd463659b1b17a75bdb0c6af.png"/></div>
+      <div className={classes.ava}><img src="https://s5o.ru/storage/simple/ru/edt/b1/c6/d7/e0/rue9685f06f6b.jpg"/></div>
       
-      <div className='posts'>
+      <div className={classes.posts}>
         My posts
-          <div>
+          <div className={classes.post}>
             New Post
           </div>
 
-          <div>
+          <div className={classes.post}>
             Post 1
           </div>
 
-          <div>
+          <div className={classes.post}>
             Post 2
           </div>
 
