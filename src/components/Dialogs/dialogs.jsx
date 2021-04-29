8e46b9717174ed3eef8a38848090c28b733115b6
Rahat Melsov <rahatmelsovforjob@gmail.com>
2021-04-30 00:00:24 +0600

    create dialogs page

--- src/components/Dialogs/dialogs.jsx
+++ src/components/Dialogs/dialogs.jsx
@@ -2,7 +2,45 @@ import React from 'react';
 import classes from './dialogs.module.css';
 
 const Dialogs = () => {
-	return <div>Dialogs</div>
+	return (
+		<div>
+			<div className={classes.dialogs}>
+				<div className={classes.dialogsItem}>
+					<div className={classes.dialog + " " + classes.active}>
+						Vlad
+					</div>
+					<div className={classes.dialog}>
+						Rahat
+					</div>
+					<div className={classes.dialog}>
+						Alex
+					</div>
+					<div className={classes.dialog}>
+						Rusa
+					</div>
+					<div className={classes.dialog}>
+						Miha
+					</div>
+					<div className={classes.dialog}>
+						Aiosh
+					</div>
+					<div className={classes.dialog}>
+						Tiko
+					</div>
+				</div>
+				<div className={classes.messages}>
+					<div className={classes.message}>
+						hi
+					</div>
+					<div className={classes.message}>
+						hi
+					</div><div className={classes.message}>
+						how are you?
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Dialogs;
