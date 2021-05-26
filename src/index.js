858625baa3900ef668ad9a39139185939865a8fc
Rahat Melsov <rahatmelsovforjob@gmail.com>
2021-05-27 00:55:28 +0600

    context encapsulation provider

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import StoreContext from './storeContext';
+import {Provider} from './storeContext';
 
 let rerender = (store) => {
   ReactDOM.render(
     <React.StrictMode>
       <BrowserRouter>
-        <StoreContext.Provider value={Store}>
+        <Provider Store={Store}>
           <App/>
-        </StoreContext.Provider>
+        </Provider>
       </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
